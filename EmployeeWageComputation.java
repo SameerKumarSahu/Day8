6c9eac4845345faf193a245085a3879dd40d58db
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 23:05:29 +0530

    Welcome Message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.EmployeeWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String [] args) {
+		
+	System.out.println("Welcome to Employee Wage Computation Program...");
+
+}
+}
