bfec600da88d4618babff957cf3e060255a176f7
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 23:44:01 +0530

    Refactor

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,24 @@
 package com.EmployeeWage;
 
+import java.util.Scanner;
+
 public class EmployeeWageComputation {
 	
-	public void condition() {
+	public int computeEmpWage(int wagePerHour,int maxHrinMonth,int numWorkingDays) {
 		//Variables
-		int wagePerHour = 20;
-		int maxHrinMonth= 100;
-		int numWorkingDays=20;
-		int totalsalary=0;
+		int totalSalary=0;
 		int totalEmpHr=0;
 		int totalWorkingDay=0;
 		int empHr=0;
-
+	
 		//Computation
 		while (totalEmpHr < maxHrinMonth && totalWorkingDay < numWorkingDays ) {
-
 				totalWorkingDay++;
-
 				int empCheck=(int) (Math.random()*3);
 
-				    switch  (empCheck) {
+				switch  (empCheck) {
 		            case 0:
-		            	empHr=0;
+		               	empHr=0;
 		            	break;
 
 		            case 1:
@@ -35,19 +32,27 @@ public class EmployeeWageComputation {
 
                 	totalEmpHr+= empHr ;
 			}
-				 	totalsalary = totalEmpHr * wagePerHour;
+				 	totalSalary = totalEmpHr * wagePerHour;
 				
 					System.out.println( "Total Number of Working days in a Month is  " + totalWorkingDay );
 					System.out.println( "Total Employee Hour in a month  is  " + totalEmpHr );
-					System.out.println( "The total Monthly Salary of the Employee is  " + totalsalary );
+					System.out.println( "The total Monthly Salary of the Employee is  " + totalSalary );
 
-		}
+		return totalSalary;
+	}
 
 	public static void main(String [] args) {
-	
+		Scanner sc = new Scanner(System.in);
 	EmployeeWageComputation obj1 = new EmployeeWageComputation();		//Creating Objects
-		obj1.condition();
+		
+		System.out.println("Enter Wage Per Hours :-");
+		int wagePerHour = sc.nextInt();
+		System.out.println("Enter the Maximun Hours in a Month :-");
+		int maxHrinMonth= sc.nextInt();
+		System.out.println("Enter Number of Working Day in a Month :-");
+		int numWorkingDays= sc.nextInt();
+		
+		obj1.computeEmpWage(wagePerHour,maxHrinMonth,numWorkingDays);
 		System.out.println("");
-	
 	}
 }
