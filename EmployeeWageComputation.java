2caf8840989bc89de7fd4e650c11d394cb946fd9
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 23:11:46 +0530

    Employee Daily Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,28 +2,28 @@ package com.EmployeeWage;
 
 public class EmployeeWageComputation {
 
-public int check() {
-	   //Variables
-	int empCheck = (int) (Math.random()*2);			//Using random check whether Employee is Present or Absent
-	int isPresent=1;
-	int isAbsent=0;
-	  //Computation
-	if (empCheck==isPresent) {
-		System.out.println("Employee is Present");
+	public int empWage() {
+		//Variables
+	int isFullTime=1;
+	int empCheck=(int) (Math.random()*2);   //Using random check whether Employee is Present or Absent
+	int empRatePerHr=8;
+	int empHrs=20;
+		//Computation
+	if (empCheck==isFullTime) {
+		int empDailyWage = empRatePerHr * empHrs;   //Employee Daily Salary
+		System.out.println("Employee is Present and Employee Fullday Wage is "+ empDailyWage);
 	}
-	
-	else if (empCheck==isAbsent) {
+	else {
 		System.out.println("Employee is Absent");
 	}	
 	return 0;
 	}
 
-	
 	public static void main(String [] args) {
-
-	EmployeeWageComputation obj1 = new EmployeeWageComputation();		//Creating Objects
 	
-	obj1.check();
+	EmployeeWageComputation obj1 = new EmployeeWageComputation();		//Creating Objects
+
+	obj1.empWage();
 	System.out.println("");
 
 	}
