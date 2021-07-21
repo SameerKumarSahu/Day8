5b6ff693c61255670a4ef32da125cf111a1707e1
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 23:08:40 +0530

    Check Employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,9 +2,29 @@ package com.EmployeeWage;
 
 public class EmployeeWageComputation {
 
-	public static void main(String [] args) {
-		
-	System.out.println("Welcome to Employee Wage Computation Program...");
+public int check() {
+	   //Variables
+	int empCheck = (int) (Math.random()*2);			//Using random check whether Employee is Present or Absent
+	int isPresent=1;
+	int isAbsent=0;
+	  //Computation
+	if (empCheck==isPresent) {
+		System.out.println("Employee is Present");
+	}
+	
+	else if (empCheck==isAbsent) {
+		System.out.println("Employee is Absent");
+	}	
+	return 0;
+	}
 
-}
+	
+	public static void main(String [] args) {
+
+	EmployeeWageComputation obj1 = new EmployeeWageComputation();		//Creating Objects
+	
+	obj1.check();
+	System.out.println("");
+
+	}
 }
