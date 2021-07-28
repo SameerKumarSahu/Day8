468e036bf6cb09a4c92221e9620039de029c492c
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-28 23:17:43 +0530

    Wage for multiple company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,75 +1,80 @@
 package com.EmployeeWage;
 
-public class EmployeeWageComputation {
-	static final int IS_FUllTime = 1;
-     static final int IS_PARTTIME = 2;
-      static String companyName;
-      int maxHrinMonth;
-      int numWorkingDays;
-      int wagePerHour;
-	
-    
-	public EmployeeWageComputation(String companyName, int maxHrinMonth, int numWorkingDays, int wagePerHour) {
-		super();
-		this.companyName = companyName;
-		this.maxHrinMonth = maxHrinMonth;
-		this.numWorkingDays = numWorkingDays;
-		this.wagePerHour = wagePerHour;
+class CompnyWage {
+
+	public String company;
+	public int wagePerHr;
+	public int maxMonthHr;
+	public int workingDays;
+	public int fullTimeHr;
+	public int partTimeHr;
+
+	public CompnyWage(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr, int partTimeHr) {
+		this.company = company;
+		this.wagePerHr = wagePerHr;
+		this.maxMonthHr = maxMonthHr;
+		this.workingDays = workingDays;
+		this.fullTimeHr = fullTimeHr;
+		this.partTimeHr = partTimeHr;
+	}
+}
+
+class EmpWageArray {
+	private static final int IS_FULL_TIME = 1;
+	private static final int IS_PART_TIME = 2;
+
+	private int numOfCompany = 0;
+	private CompnyWage[] compnyWageArray;
+
+	public EmpWageArray() {
+		compnyWageArray = new CompnyWage[5];
 	}
 
-	public int computeEmpWage() {
-		//Variables
-		int totalSalary=0;
-		int totalEmpHr=0;
-		int totalWorkingDay=0;
-		int empHr=0;
-	
-		//Computation
-		while (totalEmpHr <= maxHrinMonth && totalWorkingDay < numWorkingDays ) {
-				totalWorkingDay++;
-				int empCheck=(int) (Math.random()*3);
+	public void addCompanyEmpWage(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr,
+			int partTimeHr) {
+		compnyWageArray[numOfCompany] = new CompnyWage(company, wagePerHr, maxMonthHr, workingDays, fullTimeHr,
+				partTimeHr);
+		computeEmpWage(compnyWageArray[numOfCompany].company, compnyWageArray[numOfCompany].wagePerHr,
+				compnyWageArray[numOfCompany].maxMonthHr, compnyWageArray[numOfCompany].workingDays,
+				compnyWageArray[numOfCompany].fullTimeHr, compnyWageArray[numOfCompany].partTimeHr);
+		numOfCompany++;
+	}
 
-				switch  (empCheck) {
-		            case 0:
-		            	
-		               	empHr=0;
-		            	break;
+	public static void computeEmpWage(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr,int partTimeHr) {
+		int empHr = 0;
+		int totalEmpHr = 0;
+		int days = 0;
 
-		            case 1:
-		            	empHr=8;    //Full day hours
-						break;
+		while (empHr <= maxMonthHr && days <= workingDays) {
 
-		            case 2:
-		            	empHr=4;	// Part time hours
-		            	break;
-               }
+			double empCheck = (int) (Math.random()*3);
+			int empCheck1 = (int) empCheck;
 
-                	totalEmpHr+= empHr ;
+			switch (empCheck1) {
+			case IS_FULL_TIME:
+				empHr = fullTimeHr;
+				break;
+			case IS_PART_TIME:
+				empHr = partTimeHr;
+				break;
+			default:
+				empHr = 0;
 			}
-				 	totalSalary = totalEmpHr * wagePerHour;
-				
-					
-					return totalSalary;
-
-		
+			totalEmpHr = totalEmpHr + empHr;
+			days++;
+		}
+		int totalWage = totalEmpHr * wagePerHr;
+		System.out.println("Total Wage For " + company + " Employee Is " + totalWage);
 	}
+}
+
+public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		
-		EmployeeWageComputation company1 = new EmployeeWageComputation("Dmart",150,23,60);				//Creating Object
-		company1.computeEmpWage();
-		int a=company1.computeEmpWage();
-		System.out.println( "The total Monthly Salary of "+ companyName + " employee is  " + a+"\n" );
-		
-		EmployeeWageComputation company2 = new EmployeeWageComputation("Relience",150,200,22);
-		company2.computeEmpWage();
-		int b=company2.computeEmpWage();
-		System.out.println( "The total Monthly Salary of "+ companyName + " Employee is  " + b+"\n" );
-		
-		EmployeeWageComputation company3 = new EmployeeWageComputation("V2",200,120,26);
-		company3.computeEmpWage();
-		int c=company3.computeEmpWage();
-		System.out.println( "The total Monthly Salary of "+ companyName + " Employee is  " + c+"\n" );
+
+		EmpWageArray obj = new EmpWageArray();
+		obj.addCompanyEmpWage("D-mart", 100, 150, 26, 8, 4);
+		obj.addCompanyEmpWage("Vishal", 150, 170, 24, 10, 6);
 	}
 
 }
